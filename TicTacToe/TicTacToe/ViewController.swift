f6a81ca1a8c4f16cbc9a941cef11f4c79c957f19
Petr Martykan <petr.martykan@qubixstudio.com>
2017-10-20 11:19:25 +0200

    add

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Petr Martykan on 20.10.17.
+//  Copyright © 2017 Petr Martykan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        
+        
+    }
+
+
+}
+
